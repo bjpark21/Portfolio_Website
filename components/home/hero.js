de935f371fce4ad230263a8a96bfaac8fe0e5485
JBPARK21 <bzpark@gmail.com>
2022-12-02 11:03:00 +0900

    message

--- components/home/hero.js
+++ components/home/hero.js
@@ -6,17 +6,23 @@ export default function Hero() {
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          중부대학교 정보보호학전공
-          <br className="hidden lg:inline-block" />
-          91812323 박범주입니다.
+          중부대학교 정보보호학전공 91812323 박범주입니다.
         </h1>
+        <br />
+        <br />
         <p className="mb-8 leading-relaxed">
           중부대학교 고양캠퍼스에서 정보보호학을 전공하고있는 18학번
-          박범주입니다. 웹 어플리케이션 보안 수업을 들으며 웹에 대한 지식을
-          배워가고 있습니다. 현재 팀원들과 Fall Guys라는 팀으로 한국에 존재하는
-          다양한 축제 문화들을 하나의 사이트에서 정보를 확인할 수 있는 사이트를
-          제작하고 있습니다. 팀원들과 한 학기동안 프로젝트를 훌륭히 제 역할을
-          수행해가며 만족스러운 결과물을 낼 수 있도록 팀원들과 노력해가겠습니다.
+          박범주입니다.
+          <br />
+          <br />웹 어플리케이션 보안 수업을 들으며 웹에 대한 지식을 배워가고
+          있습니다.
+          <br /> <br />
+          현재 팀원들과 Fall Guys라는 팀으로 한국에 존재하는 다양한 축제
+          문화들을 하나의 사이트에서 정보를 확인할 수 있는 사이트를 제작하고
+          있습니다. <br />
+          <br />
+          팀원들과 한 학기동안 프로젝트를 훌륭히 제 역할을 수행해가며 만족스러운
+          결과물을 낼 수 있도록 팀원들과 노력해가겠습니다.
         </p>
         <div className="flex justify-center">
           <Link href="/projects">
